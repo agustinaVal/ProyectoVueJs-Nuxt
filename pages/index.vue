4f290909228d74850450664c93478ca5ccf73df5
Agustina <maria.agustina.valenti89@gmail.com>
2020-10-24 15:15:42 -0300

    subiendo reto

---
+++ pages/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="4" v-for="(personaje,i) in personajes" :key="i" >
+        <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+          <template slot="progress">
+            <v-progress-linear
+              color="deep-purple"
+              height="10"
+              indeterminate
+            ></v-progress-linear>
+          </template>
+
+          <v-img
+            height="250"
+            :src="personaje.imagen"
+          ></v-img>
+
+          <v-card-title>{{personaje.nombre}}</v-card-title>
+
+          <v-card-text>
+            <v-row align="center" class="mx-0">
+              <v-rating
+                :value="4.5"
+                color="amber"
+                dense
+                half-increments
+                readonly
+                size="14"
+              ></v-rating>
+              <div class="grey--text ml-4">4.5 (413)</div>
+            </v-row>
+          </v-card-text>
+          <v-divider class="mx-4"></v-divider>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-mai>
+      <Footer />
+    </v-mai>
+  </v-container>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Footer from '~/components/Footer.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo,
+    Footer
+  },
+  data() {
+    return {
+      personajes: [
+        {
+          id: 1,
+          nombre: 'Morty',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+        },
+        {
+          id: 2,
+          nombre: 'Rick',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+        },
+        {
+          id: 3,
+          nombre: 'Summer',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+        },
+        {
+          id: 4,
+          nombre: 'Beth',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
+        },
+        {
+          id: 5,
+          nombre: 'Jerry',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
+        },
+      ],
+    }
+  },
+}
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-carousel v-model="model">
+    <v-carousel-item
+      v-for="(personaje, i) in personajes"
+      :key="i"
+    >
+      <v-sheet
+        :color="color"
+        height="100%"
+        tile
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="display-3">
+           <img :src="personaje.imagen" alt="">
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+import Footer from '../components/Footer.vue'
+export default {
+  components: {
+    Footer
+  },
+  data() {
+    return {
+      personajes: [
+        {
+          id: 1,
+          nombre: 'Morty',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+        },
+        {
+          id: 2,
+          nombre: 'Rick',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+        },
+        {
+          id: 3,
+          nombre: 'Summer',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+        },
+        {
+          id: 4,
+          nombre: 'Beth',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
+        },
+        {
+          id: 5,
+          nombre: 'Jerry',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
+        },
+      ],
+    }
+  },
+}
+</script>
